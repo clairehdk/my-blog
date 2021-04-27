a46fe4fb29cbf80bd990047030023f3618d169bb
epidia <clairon_6@hotmail.fr>
2021-04-27 10:30:56 +0200

    modif build CI

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,14 @@
 const Footer = (props) => {
   return (
     <div className="footer">
-      Fait avec <span>{props.tech}</span> à <span>{props.formation}</span> par{" "}
-      <span>{props.name}</span>
+      Fait avec <span>{props.tech}</span> à{" "}
+      <a href="https://www.lereacteur.io/" target="_blank">
+        {props.formation}
+      </a>{" "}
+      par{" "}
+      <a href="https://github.com/clairehdk/" target="_blank">
+        {props.name}
+      </a>
     </div>
   );
 };
